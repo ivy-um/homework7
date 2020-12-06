878e25a00c4dfb47f1c21e5b87c3305929e7f79b
ivy-um <70938390+ivy-um@users.noreply.github.com>
2020-12-06 21:53:17 +0800

    Add files via upload

--- js/weather.js
+++ js/weather.js
@@ -1,44 +1,60 @@
 function gettingJSON(){
     //Display the forecast
     // Your code here.
+    document.querySelector("#forecast").style.display = "block";
 
     //Set default location if one isn't provided
-    let location;
+    let location = document.querySelector("#location").value || "Ann Arbor, US";
 
     // Your code here.
     console.log("Location is : " + location);
 
     //set default temperature format if one isn't provided
-    let fomat;
+    let format = document.querySelectorAll("input[name=temp]:checked")[0];
+    if (format) {
+        format = format.value;
+    } else {
+        format = "imperial";
+    }
 
     // Your code here.
     console.log("Format is " + format);
 
     //set the query  
-    let query = 'api.openweathermap.org/data/2.5/weather?q=AnnArbor,us&APPID=936e4de0cd28fde44e7c4e2f5f773f9f';
+    let query = 'https://api.openweathermap.org/data/2.5/weather?q='+location+'&units='+format+'&appid=936e4de0cd28fde44e7c4e2f5f773f9f';
+    //api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=936e4de0cd28fde44e7c4e2f5f773f9f
+    //api.openweathermap.org/data/2.5/weather?q=London&units=imperial
     // Your code here.  
     
     console.log("Query is :" + query);
 
     //Create and set variables for each of the elements you
-    //need to update, location, temp, the image, etc. 
+    //need to update, location, highs and lows, 
+    //the image, etc.
 
-    let loc;
-    let temp;
-    let tempImg;
+    let loc = document.querySelector("#loc");
+    let temp = document.querySelector("#temp");
+    let tempImg = document.querySelector("#tempImg");
     // Your code here.
 
+    //<img src="http://openweathermap.org/img/wn/01n@2x.png" width="50" height="50">
 
     $.getJSON(query,function(json){
         //Use returned json to update the values of the three 
         //elements in HTML.  
         //I would print the JSON to the console
-    console.log(JSON.stringify(json));
-       	document.querySelector("h2").innerHTML = "The temperature in " + loc + " is currently"+ temp + tempImg;
-
-    });
-  });
+        // Your code here.
+	    //Use json to update the values.  I would 
+	    //print it to the console
+        console.log(JSON.stringify(json));
+        //console.log(json.weather[0].main)
+        //console.log(json.weather[0].description)
+        //console.log(json.weather[0].icon)
+        loc.innerHTML = json.name;
+        temp.innerHTML = json.main.temp + " with " + json.weather[0].description;
+        tempImg.src ="http://openweathermap.org/img/wn/" + json.weather[0].icon + "@2x.png";
+        tempImg.alt = json.weather[0].description;
+        tempImg.title = "weather icon";
 
     });
 }
-
